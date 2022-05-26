df31a29061d7b9093daa7dce2005eb59356f6d86
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-05-26 17:22:07 -0500

    Cleanup

--- src/main/java/com/jetpacker06/fueltool/FuelTool.java
+++ src/main/java/com/jetpacker06/fueltool/FuelTool.java
@@ -2,15 +2,12 @@ package com.jetpacker06.fueltool;
 
 import com.google.gson.Gson;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.lwjgl.system.CallbackI;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -36,22 +33,41 @@ public class FuelTool {
                 writer.close();
                 log("Created custom_fuels.json");
             }
-            else {log("Reading custom_fuels.json...");}
+            else {
+                log("Reading custom_fuels.json...");
+            }
             Scanner reader = new Scanner(listJson);
             String rawText = "";
             while (reader.hasNextLine()) {
                 rawText = rawText + reader.nextLine();
             }
-            if (rawText.equals("")) rawText = "{}";
-
-            Map<String, Double> map = new Gson().fromJson(rawText, Map.class);
-            for (Map.Entry<String, Double> entry : map.entrySet()) {
-
+            if (rawText.equals("")) {
+                rawText = "{}";
             }
+            Map<String, Double> map = new Gson().fromJson(rawText, Map.class);
             return map;
         } catch (IOException e) {
             log("An error occurred involving custom_fuels.json. Go to the config folder and delete custom_fuels.json, then rerun the game.");
         }
         return null;
+    }
+
+    @SuppressWarnings("unused")
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public static class HandlingEvents {
+        public static void log(Object message) {
+            LOGGER.info(message);}
+        @SubscribeEvent
+        public static void addFuels(FurnaceFuelBurnTimeEvent event) {
+            String[] list = fuelsMap.keySet().toArray(new String[0]);
+            for(int i = 0; i < list.length; i++) {
+                if (list[i].toLowerCase().startsWith(event.getItemStack().getDescriptionId().toLowerCase())) {
+                    double value = fuelsMap.get(event.getItemStack().getDescriptionId());
+                    event.setBurnTime((int) Math.round(fuelsMap.get(event.getItemStack().getDescriptionId())));
+                    log("Set burn time of " + event.getItemStack().getDescriptionId() + "to" + fuelsMap.get(event.getItemStack().getDescriptionId()));
+                    break;
+                }
+            }
+        }
     }
 }
